fb47d024e042602d158ff2b5c35db76b616f89d6
Atthawit <87508680+atthawit-ratakaset@users.noreply.github.com>
2023-05-18 16:13:14 +0700

    test

--- My project/Assets/Scripts/Button.cs
+++ My project/Assets/Scripts/Button.cs
@@ -10,11 +10,13 @@ public class Button : MonoBehaviour
     int CurrenttIndex = 0;
     private bool hit;
     private bool end;
+    public KeyCode key;
     private SpriteRenderer theSR;
     private bool subNote1;
     private bool subNote2;
     private bool subNote3;
     public GameObject goodEffect, perfectEffect, missEffect, badEffect;
+    public ParticleSystem particEffect;
 
     void Awake() {
         theSR = GetComponent<SpriteRenderer>();
@@ -32,10 +34,45 @@ public class Button : MonoBehaviour
     }
 
     void Update() {
-    }
+        if (Input.GetKeyDown(key)) {
+            particEffect.Play();
+            if (hit == true) {
+                if ((subNote1 == true && subNote2 == true && subNote3 == true)) {
+                    // Debug.Log("Perfect");
+                    Score.instance.AddPerfectScore();
+                    Score.instance.AddCombo();
+                    Instantiate(perfectEffect, new Vector3(0f, 5.2f, 0f), Quaternion.identity);
+                } else if ((subNote1 == true && subNote2 == false && subNote3 == false) || 
+                            (subNote3 == true && subNote2 == false && subNote3 == false)){
+                    // Debug.Log("Good");
+                    Instantiate(badEffect, new Vector3(0f, 5.2f, 0f), Quaternion.identity);
+                    Score.instance.AddBadScore();
+                    Score.instance.AddCombo();
+                } else {
+                    Score.instance.AddGoodScore();
+                    Score.instance.AddCombo();
+                    Instantiate(goodEffect, new Vector3(0f, 5.2f, 0f), Quaternion.identity);
+                }
+                
+                if (end == true) {
+                    this.Wait(2f, ShowScore);
+                    MusicScript.instance.DestroyMusic();
+                }
+                DestroyNote();
+                
 
+
+            } else {
+                // Debug.Log("Miss");
+                Score.instance.AddMissScore();
+                Instantiate(missEffect, new Vector3(0f, 5.2f, 0f), Quaternion.identity);
+                Score.instance.ReSetCombo();
+            }            
+        }
+    }
  
     void OnMouseDown() {
+        particEffect.Play();
         if (hit == true) {
             if ((subNote1 == true && subNote2 == true && subNote3 == true)) {
                 // Debug.Log("Perfect");
